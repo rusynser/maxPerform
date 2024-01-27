6b2215e13a7b4c01b182793aa34704870bb5ea96
rusynser <98419606+rusynser@users.noreply.github.com>
2024-01-27 19:57:36 +0100

    gg
    ggg

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -2,6 +2,7 @@
 import ProjectDao from '../dao/projectDao.js';
 import { ObjectId } from 'mongodb';
 import TaskDao from '../dao/taskDao.js';
+import UserDao from '../dao/userDao.js';
 
 export const createProject = async (req, res) => {
     try {
@@ -82,5 +83,32 @@ export const getTasksForProject = async (req, res) => {
         res.json(tasks);
     } catch (error) {
         res.status(500).json({ message: error.message });
+    }
+};
+
+export const addUserToProject = async (req, res) => {
+    const { projectId } = req.params;
+    const { username } = req.body;
+
+    try {
+        const user = await UserDao.findUserByUsername(username);
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+
+        // Check if the user is a freelancer
+        if (user.role !== 'freelancer') {
+            return res.status(403).send('User is not a freelancer');
+        }
+
+        const userId = user._id;
+        const result = await ProjectDao.addUserToProject(projectId, userId);
+        if (result.modifiedCount === 0) {
+            return res.status(404).send('Project not found or User already added');
+        }
+
+        res.status(200).send('User added to project successfully');
+    } catch (error) {
+        res.status(500).send('Error adding user to project: ' + error.message);
     }
 };
